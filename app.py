6d8503fea5fea1e4370f2b49c89a9ffe360f92e6
Afonso Gomes <afonso.gomes@tecnico.ulisboa.pt>
2024-10-09 16:16:15 +0100

    Added host="0.0.0.0"

--- app.py
+++ app.py
@@ -148,4 +148,4 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-p", "--port", default=8000, type=int, help="port to listen on")
     args = parser.parse_args()
-    app.run(port=args.port, debug=True)
+    app.run(host="0.0.0.0", port=args.port, debug=True)
